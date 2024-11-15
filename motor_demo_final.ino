d8869c8c2b9e5c45716c7751149cdc1a746ceffc
Dylan Bautista <70183425+DylanBaut@users.noreply.github.com>
2024-11-14 18:24:09 -0600

    Create motor_demo_final.ino

---
+++ motor_demo_final.ino
@@ -0,0 +1,53 @@
+#include <Servo.h>
+
+Servo myservo; 
+ 
+int pos = 0;
+
+int zeroThrottle = 90; //90 Needs to be between 66 and 101.
+int maxThrottle = 140; //140 seems to be where the maximum is. Above this doesn't give any extra speed.
+int minThrottle = 60;  //60 This represents the "reverse" speed.
+ 
+void setup()
+{
+
+Serial.begin(9600); //Pour some serial
+
+myservo.attach(5); // attaches the servo on pin  to the servo object
+}
+ 
+void loop()
+{
+//  myservo.write(100);
+//  Serial.println("100");
+//
+//  delay(5000);
+//  myservo.write(120);
+//   Serial.println("120");
+//  delay(5000);
+//  myservo.write(130);
+//   Serial.println("130");
+//
+//  delay(5000);
+//   myservo.write(140);
+//   Serial.println("140");
+//  delay(5000);
+  
+ 
+Serial.println("ZERO TO MAX");
+for(pos = zeroThrottle; pos < maxThrottle; pos += 1)  // goes from zero to max throttle
+  {                                  
+    myservo.write(pos);               // tell the ESC to position in variable 'pos'
+    delay(200);                       // waits 200ms for the ESC to reach the position
+    Serial.println(pos);              // print "pos"
+  }
+
+Serial.println("TRIGGER ON");
+myservo.write(89);               //APPARENTLY WRITING A VALUE ANYTHING LESS THAN ZERO THROTTLE FOR A SECOND OR TWO WILL TRIGGER THE ESC ON
+                                  // THE ESC DOESNT EVEN NEED TO BE LESS THAN ZERO THROTTLE ON THE ESC'S STARTUP, JUST ANY POINT WILL TRIGGER IT ON
+delay(2000);
+
+
+
+  
+}
